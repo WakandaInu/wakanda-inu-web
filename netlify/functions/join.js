bebae7d3554b7e5ca2cb5f9ca99339708ce5d07b
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2021-11-28 09:57:36 +0100

    feat: Add recaptcha v3

--- netlify/functions/join.js
+++ netlify/functions/join.js
@@ -1,6 +1,26 @@
 const { getFirestore } = require("../firestore");
 const firestore = getFirestore();
 
+async function verifyCaptcha(token) {
+  try {
+    const response = await fetch(
+      "https://www.google.com/recaptcha/api/siteverify",
+      {
+        method: "POST",
+        body: JSON.stingify({
+          secret: "6Le5WmQdAAAAAFbd83pwuxlMim6jT0-V0I6duFEO",
+          response: token,
+        }),
+      }
+    );
+    const json = response.json();
+
+    return Promise.resolve(json.success);
+  } catch {
+    return Promise.resolve(false);
+  }
+}
+
 exports.handler = async function join(event, context, callback) {
   const { httpMethod, body } = event;
 
@@ -14,6 +34,24 @@ exports.handler = async function join(event, context, callback) {
   try {
     const parsedBody = JSON.parse(body);
     const { email } = parsedBody;
+
+    if (!parsedBody["g-recaptcha-response"]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Invalid request" }),
+      };
+    }
+
+    const hasValidCaptcha = await verifyCaptcha(
+      parsedBody["g-recaptcha-response"]
+    );
+
+    if (!hasValidCaptcha) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Invalid request" }),
+      };
+    }
 
     if (!email) {
       return {
@@ -50,7 +88,6 @@ exports.handler = async function join(event, context, callback) {
       body: JSON.stringify({ message: "OK" }),
     };
   } catch (error) {
-    console.log(error);
     return {
       statusCode: 500,
       body: JSON.stringify({
